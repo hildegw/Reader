5e0ca9e26c21dc686d1c27836a247e0659b1a410
hildegw <hildegw@snafu.de>
2017-09-06 18:09:27 -0500

    removed history{/} in App, refactored author list and moved styles in ShowBooks

--- src/ShowBook.js
+++ src/ShowBook.js
@@ -51,9 +51,8 @@ class ShowBook extends Component{
        <div className="book">
          <div className="book-top">
            <div className="book-cover"
-            style={{ width: 128, height: 193,
-              backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}>
-           </div>
+            style={{backgroundImage: `url(${book.imageLinks.smallThumbnail})`}}
+           />
            <div className="book-shelf-changer">
              <select onClick={(event)=>
                 this.submitBookToShelf(event.target.value, book)}>
@@ -75,8 +74,9 @@ class ShowBook extends Component{
          </div>
          <div className="book-shelf-info">{this.state.bookShelf}</div>
          <div className="book-title">{book.title}</div>
-         { book.authors !== undefined && (book.authors.map((author)=>
-           <div className="book-authors" key={author} >{author}</div>))}
+         <div className="book-authors" >
+          { book.authors ? book.authors.join(", ") : "" }
+         </div>
        </div>
 )}}
 
